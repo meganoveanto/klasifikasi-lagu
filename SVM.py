0b7eb1dd70561a86c2100ae2346278fd4932497b
meganoveanto <52860668+meganoveanto@users.noreply.github.com>
2022-06-11 15:01:42 +0700

    Add files via upload

--- SVM.py
+++ SVM.py
@@ -4,6 +4,8 @@ import pandas as pd
 import re
 import numpy as np
 import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
